ad4141eada03df3513cbe22fa25885273e5cb203
RyuJeongSang <42318201+RyuJeongSang@users.noreply.github.com>
2022-12-02 14:59:30 +0900

    angular init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tutorial';
+
+  constructor (){  //constructor 라는 함수를 추가합니다.
+    console.log(`angular server is running in http://localhost:4200`);
+  }
 }
