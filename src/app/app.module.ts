1f1975154273cb131da24dc32c983f09d090e710
RyuJeongSang <42318201+RyuJeongSang@users.noreply.github.com>
2022-12-06 21:07:29 +0900

    angular form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloworldComponent } from './helloworld/helloworld.component';
 import { IsvisibleComponent } from './isvisible/isvisible.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HelloworldComponent,
-    IsvisibleComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HelloworldComponent, IsvisibleComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/isvisible/isvisible.component.ts
+++ src/app/isvisible/isvisible.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-isvisible',
   templateUrl: './isvisible.component.html',
-  styleUrls: ['./isvisible.component.css']
+  styleUrls: ['./isvisible.component.css'],
 })
 export class IsvisibleComponent {
+  id!: string;
+  password!: string;
 
+  onSubmit = () => {
+    console.log(this.id, this.password);
+  };
 }
